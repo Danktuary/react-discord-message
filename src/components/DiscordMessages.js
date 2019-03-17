e237d4e60893267318784c90528aaf930a21480d
Sanctuary <sanc.pw@gmail.com>
2019-03-17 19:27:11 -0400

    Update DiscordMessages slot rendering

--- src/components/DiscordMessages.js
+++ src/components/DiscordMessages.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './DiscordMessages.css';
 
+export default ({ children, compactMode = false, lightTheme = false }) => {
 	let classes = 'discord-messages';
 
 	if (lightTheme) classes += ' discord-light-theme';
 	if (compactMode) classes += ' discord-compact-mode';
 
+	const makeCompact = element => React.cloneElement(element, { compactMode });
+	const messages = !Array.isArray(children) ? makeCompact(children) : children.map(makeCompact);
+
 	return (
 		<div className={classes}>
-			{children}
+			{messages}
 		</div>
 	);
 };
