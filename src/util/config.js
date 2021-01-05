c07bcb9cc628d862b19f5902aae460da88b3bf00
Sanctuary <sanc.pw@gmail.com>
2021-01-05 02:28:29 -0500

    Remove internal avatar assets, use CDN links instead

--- src/util/config.js
+++ src/util/config.js
@@ -1,11 +1,11 @@
-import blue from '../avatars/blue.png';
-import gray from '../avatars/gray.png';
-import green from '../avatars/green.png';
-import orange from '../avatars/orange.png';
-import red from '../avatars/red.png';
-
 const config = {
-	avatars: { blue, gray, green, orange, red },
+	avatars: {
+		blue: 'https://cdn.discordapp.com/attachments/654503812593090602/665721745466195978/blue.png',
+		gray: 'https://cdn.discordapp.com/attachments/654503812593090602/665721746569166849/gray.png',
+		green: 'https://cdn.discordapp.com/attachments/654503812593090602/665721748431306753/green.png',
+		orange: 'https://cdn.discordapp.com/attachments/654503812593090602/665721750201434138/orange.png',
+		red: 'https://cdn.discordapp.com/attachments/654503812593090602/665721752277483540/red.png',
+	},
 	profiles: {},
 };
 
