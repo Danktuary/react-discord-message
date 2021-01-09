c68838ae82b696d26e2961a3fe824b1d779dd8ce
Sanctuary <sanc.pw@gmail.com>
2021-01-09 16:24:41 -0500

    Add production umd build

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,67 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
+import peerExternals from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
 import url from '@rollup/plugin-url'
 import del from 'rollup-plugin-delete'
 import svgr from '@svgr/rollup'
 import pkg from './package.json'
 
-export default {
-	input: 'src/index.js',
-	output: [
-		{
-			file: pkg.main,
-			format: 'cjs',
-			sourcemap: true,
-		},
-		{
-			file: pkg.module,
-			format: 'es',
-			sourcemap: true,
-		},
-	],
-	plugins: [
-		external(),
-		postcss(),
-		url(),
-		svgr(),
-		babel({
-			babelHelpers: 'runtime',
-			exclude: 'node_modules/**',
-		}),
-		del({ targets: ['dist/*'] }),
-		resolve(),
-		commonjs(),
-	],
+const plugins = [
+	peerExternals(),
+	postcss(),
+	url(),
+	svgr(),
+	babel({
+		babelHelpers: 'runtime',
+		exclude: 'node_modules/**',
+	}),
+	resolve(),
+	commonjs(),
+]
+
+const config = [
+	{
+		input: 'src/index.js',
+		output: [
+			{
+				file: pkg.main,
+				format: 'cjs',
+				sourcemap: true,
+			},
+			{
+				file: pkg.module,
+				format: 'es',
+				sourcemap: true,
+			},
+		],
+		plugins: [del({ targets: ['dist/*'] })].concat(...plugins),
+	},
+]
+
+if (process.env.NODE_ENV === 'production') {
+	config.push({
+		input: 'src/index.js',
+		output: [
+			{
+				file: pkg.main.replace(/\.js$/, '.min.js'),
+				format: 'umd',
+				name: 'ReactDiscordMessage',
+				plugins: [terser()],
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+					'prop-types': 'PropTypes',
+				},
+			},
+		],
+		plugins: plugins.concat(replace({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+		})),
+	})
 }
+
+export default config
